40abffa5ed8c99c8a9f43d77ced3280ce55cbf6d
try-catch-stack <77742477+try-catch-stack@users.noreply.github.com>
2021-08-23 23:42:15 +0530

    Fix favicon bug

--- pages/_document.js
+++ pages/_document.js
@@ -38,7 +38,11 @@ export default class MyDocument extends Document {
           />
           <script src="https://cdn.lordicon.com/libs/mssddfmo/lord-icon-2.1.0.js" />
 
-          <link rel="icon" href="../public/favicon.ico" type="image/x-icon" />
+          {/* <link
+            rel="icon"
+            href="../public/favicon.ico"
+            type="image/x-icon"
+          ></link> */}
         </Head>
         <body>
           <Main />
