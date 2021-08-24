426b18da3c88f501eaa1e4073714bdbbf12f7d9e
Danish Ahmed Mirza <77742477+try-catch-stack@users.noreply.github.com>
2021-08-24 19:23:08 +0530

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -19,7 +19,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Rampart+One&display=swap"
             rel="stylesheet"
